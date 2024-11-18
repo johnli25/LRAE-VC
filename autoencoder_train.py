c661ea57ed2c1a6ed41389e83a4470bba1395c57
John Li <warriorjohn36@gmail.com>
2024-11-18 00:57:40 -0600

    push

--- autoencoder_train.py
+++ autoencoder_train.py
@@ -8,6 +8,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import defaultdict
+import argparse
 
 
 
@@ -125,38 +126,25 @@ class PNC_Autoencoder(nn.Module):
     def forward(self, x):
         # Encoder
         x1 = self.relu(self.encoder1(x))  # (16, 32, 32)
-        # print(f"Shape after encoder1: {x1.shape}")
         x2 = self.relu(self.encoder2(x1))  # (10, 32, 32)
-        # print(f"Shape after encoder2: {x2.shape}")
 
         # Decoder
         y1 = self.relu(self.decoder1(x2))  # (64, 224, 224)
-        # print(f"Shape after decoder1: {y1.shape}")
-
         y2 = self.relu(self.decoder2(y1))  # (64, 224, 224)
-        # print(f"Shape after decoder2: {y2.shape}")
         y2 = y2 + y1 # Skip connection
-
         y3 = self.relu(self.decoder3(y2))  # (64, 224, 224)
-        # print(f"Shape after decoder3: {y3.shape}")
-
         y4 = self.relu(self.decoder3(y3))  # (64, 224, 224)
-        # print(f"Shape after decoder3 (second time): {y4.shape}")
         y4 = y4 + y3  # Skip connection
-
         y5 = self.final_layer(y4)  # (3, 224, 224)
-        # print(f"Shape after final_layer: {y5.shape}")
         y5 = torch.clamp(y5, min=0, max=1)  # Ensure output is in [0, 1] range
-        return y5
 
-'''
-Used Deeper Encoder
-Leaky ReLU: Avoids dead neurons, especially with small gradients.
-Dropout: Reduces the chance of overfitting, especially in the decoder.
-'''
-class PNC_Autoencoder_Modified(nn.Module):
+        return y5 # Return (decoded image, classification output label)
+    
+
+
+class LRAE_VC_Autoencoder(nn.Module):
     def __init__(self):
-        super(PNC_Autoencoder_Modified, self).__init__()
+        super(LRAE_VC_Autoencoder, self).__init__()
 
         # Encoder
         self.encoder1 = nn.Sequential(
@@ -218,6 +206,59 @@ class PNC_Autoencoder_Modified(nn.Module):
 
 
 
+class PNC_Autoencoder_with_Classification(nn.Module):
+    def __init__(self, num_classes=10, classes=['diving', 'golf_front', 'kick_front', 'lifting', 'riding_horse', 'running', 'skating', 'swing_bench', 'swing_side', 'walk_front']): # Default classes derived from UCF-101
+        super(PNC_Autoencoder_with_Classification, self).__init__()
+
+        self.classes = classes
+
+        # Encoder
+        self.encoder1 = nn.Conv2d(3, 16, kernel_size=9, stride=7, padding=4)  # (3, 224, 224) -> (16, 32, 32)
+        self.encoder2 = nn.Conv2d(16, 10, kernel_size=3, stride=1, padding=1)  # (16, 32, 32) -> (10, 32, 32)
+
+        # Classification
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(10 * 32 * 32, 256),  # Flattened bottleneck size -> 256 hidden units
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, num_classes)  # Final layer for classification
+        )
+
+        # Decoder
+        self.decoder1 = nn.ConvTranspose2d(10, 64, kernel_size=9, stride=7, padding=4, output_padding=6)  # (10, 32, 32) -> (64, 224, 224)
+        self.decoder2 = nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=2)  # (64, 224, 224) -> (64, 224, 224)
+        self.decoder3 = nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=2)  # (64, 224, 224) -> (64, 224, 224)
+        self.final_layer = nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1)  # (64, 224, 224) -> (3, 224, 224)
+
+        # Activation Functions
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()  # For output normalization in range [0, 1]
+
+    def forward(self, x):
+        # Encoder
+        x1 = self.relu(self.encoder1(x))  # (16, 32, 32)
+        x2 = self.relu(self.encoder2(x1))  # (10, 32, 32)
+
+        # Decoder
+        y1 = self.relu(self.decoder1(x2))  # (64, 224, 224)
+        y2 = self.relu(self.decoder2(y1))  # (64, 224, 224)
+        y2 = y2 + y1 # Skip connection
+        y3 = self.relu(self.decoder3(y2))  # (64, 224, 224)
+        y4 = self.relu(self.decoder3(y3))  # (64, 224, 224)
+        y4 = y4 + y3  # Skip connection
+        y5 = self.final_layer(y4)  # (3, 224, 224)
+        y5 = torch.clamp(y5, min=0, max=1)  # Ensure output is in [0, 1] range
+
+        # Classification
+        class_scores = self.classifier(x2)  # (10, 32, 32) -> (num_classes)
+
+        return y5, class_scores # Return (decoded image, class output label)
+
+
+
+
+
 # Dataset class for loading images and ground truths
 class ImageDataset(Dataset):
     def __init__(self, img_dir, gt_dir, transform=None):
@@ -242,22 +283,49 @@ class ImageDataset(Dataset):
 
         return image, ground_truth, self.img_names[idx]
 
-#Testing and training
-def train_autoencoder(model, dataloader, criterion, optimizer, device):
-    model.train()
-    train_loss = 0
-    for inputs, targets, _ in dataloader:
-        inputs, targets = inputs.to(device), targets.to(device)
 
-        optimizer.zero_grad()
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
+# Testing and training
+def train_autoencoder(model, train_loader, val_loader, criterion, optimizer, device, num_epochs, model_name):
+    best_val_loss = float('inf')
+    train_losses, val_losses = [], []
+    for epoch in range(num_epochs):
+        # Train the model
+        model.train()
+        train_loss = 0
+        for inputs, targets, _ in train_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
 
-        train_loss += loss.item() * inputs.size(0)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
 
-    return train_loss / len(dataloader.dataset)
+            train_loss += loss.item() * inputs.size(0)
+
+        train_loss /= len(train_loader.dataset)
+        train_losses.append(train_loss)
+
+        # Validate the model
+        val_loss = test_autoencoder(model, val_loader, criterion, device)
+        val_losses.append(val_loss)
+
+        # Save the best model
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            torch.save(model.state_dict(), f"{model_name}_best_validation.pth")
+            print(f"Epoch [{epoch+1}/{num_epochs}]: Validation loss improved. Model saved.")
+
+        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}")
+
+    # Final Test
+    test_loss = test_autoencoder(model, test_loader, criterion, device)
+    print(f"Final Test Loss: {test_loss:.4f}")
+
+    plot_train_val_loss(train_losses, val_losses) # Plot training and validation loss over time/epochs
+
+    # Save final model
+    torch.save(model.state_dict(), f"{model_name}_final.pth")
 
 def test_autoencoder(model, dataloader, criterion, device):
     model.eval()
@@ -270,6 +338,113 @@ def test_autoencoder(model, dataloader, criterion, device):
             test_loss += loss.item() * inputs.size(0)
 
     return test_loss / len(dataloader.dataset)
+
+
+
+
+def train_combined_model(model, train_loader, val_loader, test_loader, criterion_reconstruction, criterion_classification, optimizer, device, num_epochs, model_name):
+    print(f"Training {model_name} with combined loss...")
+    best_val_loss = float('inf')
+
+    for epoch in range(num_epochs):
+        # Train the model
+        model.train()
+        train_loss = 0
+        for inputs, targets, _ in train_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            optimizer.zero_grad()
+            outputs, class_scores = model(inputs)
+
+            # Reconstruction loss
+            loss_reconstruction = criterion_reconstruction(outputs, targets)
+
+            # Classification loss
+            labels = get_labels_from_filename(_)
+            labels = torch.tensor(labels).to(device)
+            loss_classification = criterion_classification(class_scores, labels)
+
+            # Combined loss
+            loss = loss_reconstruction + loss_classification
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item() * inputs.size(0)
+
+        train_loss /= len(train_loader.dataset)
+
+        # Validate the model
+        val_loss = test_combined_model(model, val_loader, criterion_reconstruction, criterion_classification, device)
+
+        # Save the best model
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            torch.save(model.state_dict(), f"{model_name}_best_validation.pth")
+            print(f"Epoch [{epoch+1}/{num_epochs}]: Validation loss improved. Model saved.")
+
+        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}")
+
+        torch.cuda.empty_cache()
+
+    # Final Testing
+    test_loss = test_combined_model(model, test_loader, criterion_reconstruction, criterion_classification, device)
+    print(f"Final Test Loss for {model_name}: {test_loss:.4f}")
+
+    # Save final model
+    torch.save(model.state_dict(), f"{model_name}_final.pth")
+
+def test_combined_model(model, dataloader, criterion_reconstruction, criterion_classification, device, final_test=False):
+    model.eval()
+    test_loss = 0
+    all_predictions = []
+    filenames_list = []
+    with torch.no_grad():
+        for inputs, targets, img_file_name in dataloader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs, class_scores = model(inputs)
+
+            # Reconstruction loss
+            loss_reconstruction = criterion_reconstruction(outputs, targets)
+
+            # Classification loss
+            labels = get_labels_from_filename(img_file_name)
+            labels = torch.tensor(labels).to(device)
+            loss_classification = criterion_classification(class_scores, labels)
+
+            # Combined loss
+            loss = loss_reconstruction + loss_classification
+            test_loss += loss.item() * inputs.size(0)
+
+            # Predict class labels
+            _, predicted_labels = torch.max(class_scores, 1) # shape is (batch_size,) since it's batch_size of predictions
+            all_predictions.extend(predicted_labels.cpu().numpy()) # ... that's why use .extend() because it's a list of predictions
+            filenames_list.extend(img_file_name)
+
+    if final_test:
+        # Print predictions
+        for filename, prediction in zip(filenames_list, all_predictions):
+            print(f"Image: {filename}, Predicted Label: {prediction}")
+
+    return test_loss / len(dataloader.dataset)
+
+
+
+def get_labels_from_filename(filenames, class_map = {
+        "diving": 0, "golf_front": 1, "kick_front": 2, "lifting": 3, "riding_horse": 4,
+        "running": 5, "skating": 6, "swing_bench": 7, "swing_side": 8, "walk_front": 9
+    }):
+    labels = []
+    for filename in filenames:
+        activity = "_".join(filename.split("_")[:-2])
+        labels.append(class_map[activity])
+    return labels
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train the PNC Autoencoder or PNC Autoencoder with Classification.")
+    parser.add_argument("--model", type=str, required=True, choices=["PNC", "PNC_with_classification", "LRAE_VC"], 
+                        help="Model to train")
+    return parser.parse_args()
 
 
 def plot_train_val_loss(train_losses, val_losses):
@@ -285,11 +460,13 @@ def plot_train_val_loss(train_losses, val_losses):
     plt.grid(alpha=0.3)
     plt.tight_layout()
 
-    plt.savefig(save_path+'train_val_loss_curve.png', dpi=300)
+    plt.savefig('train_val_loss_curve.png', dpi=300)
     plt.show()
 
-    
+
 if __name__ == "__main__":
+    args = parse_args()
+
     ## Hyperparameters
     num_epochs = 30
     batch_size = 32
@@ -356,55 +533,51 @@ if __name__ == "__main__":
     # Model, criterion, optimizer
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
-    model = PNC_Autoencoder_Modified().to(device)
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    train_losses = []
-    val_losses = []
-    save_path = ''
-    # Training loop with validation
-    best_val_loss = float('inf')
-    for epoch in range(num_epochs):
-        # Train the model
-        train_loss = train_autoencoder(model, train_loader, criterion, optimizer, device)
-        train_losses.append(train_loss)
-        # Validate the model
-        val_loss = test_autoencoder(model, val_loader, criterion, device)
-        val_losses.append(val_loss)
-        # Save the best model
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            torch.save(model.state_dict(), save_path+"best_validation_autoencoder.pth")
-            print(f"Epoch [{epoch+1}/{num_epochs}]: Validation loss improved. Model saved.")
+
+
+    if args.model == "PNC":
+        model = PNC_Autoencoder().to(device)
+        criterion = nn.MSELoss()
+        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        train_autoencoder(model, train_loader, val_loader, criterion, optimizer, device, num_epochs, args.model)
         
-        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}")
-
-    # Final test evaluation
-    test_loss = test_autoencoder(model, test_loader, criterion, device)
-    print(f"Final Test Loss: {test_loss:.4f}")
-
-    # Save the final model
-    torch.save(model.state_dict(), save_path+"autoencoder_final.pth")
+    if args.model == "LRAE_VC":
+        model = LRAE_VC_Autoencoder().to(device)
+        criterion = nn.MSELoss()
+        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        train_autoencoder(model, train_loader, val_loader, criterion, optimizer, device, num_epochs, args.model)
+    
+        # Call the respective training function for autoencoder
+    if args.model == "PNC_with_classification":
+        model = PNC_Autoencoder_with_Classification().to(device)
+        criterion_reconstruction = nn.MSELoss()
+        criterion_classification = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        train_combined_model(model, train_loader, val_loader, test_loader, criterion_reconstruction, criterion_classification, optimizer, device, num_epochs, args.model)
 
 
 
     # Save images generated by decoder 
-    output_path = "output_imgs/"
+    output_path = "output_test_imgs_post_training/"
     if not os.path.exists(output_path):
+        print(f"Creating directory: {output_path}")
         os.makedirs(output_path)
+    else:
+        print(f"Directory already exists: {output_path}")
+
 
     model.eval()
-    print(f"Total number of batches in test_loader: {len(test_loader)}")
-    print(f"Total number of samples in test_dataset: {len(test_loader.dataset)}")
     with torch.no_grad():
         for i, (inputs, _, filenames) in enumerate(test_loader):
             inputs = inputs.to(device)
-            outputs = model(inputs)
+            if args.model == "PNC_with_classification": outputs, _ = model(inputs)
+            else: outputs = model(inputs)
             for j in range(inputs.size()[0]):
                 output = outputs[j].permute(1, 2, 0).cpu().numpy() # outputs[j] original shape is (3, 224, 224), which need to convert to -> (224, 224, 3)
                 # output = (output * 255).astype(np.uint8)
 
                 # save the numpy array as image
                 plt.imsave(os.path.join(output_path, filenames[j]), output)
-    plot_train_val_loss(train_losses, val_losses)
+
+
