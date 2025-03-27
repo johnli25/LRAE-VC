c89183d883d7e130c07a2eb1b6720a2a2d3ff420
John Li <warriorjohn36@gmail.com>
2025-03-26 23:31:34 -0500

    update input argument, drops, in evaluate() to actually take in args.drop instead of a hardcoded value

--- autoencoder_train_vid_sequence.py
+++ autoencoder_train_vid_sequence.py
@@ -449,7 +449,7 @@ if __name__ == "__main__":
 
 
     # NOTE: for Experimental Evaluation
-    # final_test_loss = evaluate(model, test_loader, criterion, device, save_sample="test", drop=0, quantize=args.quantize) # constant number of drops
-    final_test_loss = evaluate_realistic(model, test_loader, criterion, device, input_drop=args.drops) # random number of drops
+    final_test_loss = evaluate(model, test_loader, criterion, device, save_sample="test", drop=args.drops, quantize=args.quantize) # constant number of drops
+    # final_test_loss = evaluate_realistic(model, test_loader, criterion, device, input_drop=args.drops) # random number of drops
     print(f"Final Test Loss For evaluation: {final_test_loss:.4f}")
     
