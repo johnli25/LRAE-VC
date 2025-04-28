db1d25c126207bb5ee7a0276fa14ac502d0897d8
John Li <warriorjohn36@gmail.com>
2025-04-27 20:38:53 -0500

    mahimahi finally kinda works??

--- sender.sh
+++ sender.sh
@@ -3,7 +3,7 @@ python sender_encode.py \
   --model        pnc32 \
   --model_path   PNC32_final_w_taildrops.pth \
   --input        ./TUCF_sports_action_224x224_mp4_vids \
-  --ip      100.0.0.4  \
+  --ip      10.0.0.2 \
   --port    9000 \
   # --lstm_kwargs  '{"total_channels":32,"hidden_channels":32,"ae_model_name":"PNC32","bidirectional":false}' \
   # --quant False
