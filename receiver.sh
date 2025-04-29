9f130aefcccefa555aea44c059c651c68f66c1f8
John Li <warriorjohn36@gmail.com>
2025-04-29 17:20:44 -0500

    train on NCSA server

--- receiver.sh
+++ receiver.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-python -u receiver_decode.py \
+python receiver_decode.py \
   --model        pnc32 \
-  --model_path   PNC32_final_w_taildrops.pth \
+  --model_path   PNC32_final_no_dropouts_quantize256_8bits.pth \
   --ip      0.0.0.0 \
   --port    9000 \
   --deadline_ms 1300 \
   # --lstm_kwargs  '{"total_channels":32,"hidden_channels":32,"ae_model_name":"PNC32","bidirectional":false}' \
-  # --quant False
+  --quant
 
 
 ##### copy from below for ease of access:
@@ -15,3 +15,6 @@ python -u receiver_decode.py \
 # conv_lstm_PNC32_ae
 # conv_lstm_PNC32_ae_dropUpTo_32_final_pristine.pth
 # conv_lstm_PNC32_ae_dropUpTo_32_bidirectional_final.pth
+# conv_lstm_PNC32_ae_dropUpTo_32_quantize256_8_bits.pth
+# PNC32_final_no_dropouts_quantize256_8bits.pth
+# conv_lstm_PNC32_ae_dropUpTo_32_quantize256_8_bits.pth
--- sender.sh
+++ sender.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 python sender_encode.py \
   --model        pnc32 \
-  --model_path   PNC32_final_w_taildrops.pth \
+  --model_path   PNC32_final_no_dropouts_quantize256_8bits.pth \
   --input        ./TUCF_sports_action_224x224_mp4_vids \
-  --ip      10.0.0.2 \
+  --ip      10.0.0.1 \
   --port    9000 \
   # --lstm_kwargs  '{"total_channels":32,"hidden_channels":32,"ae_model_name":"PNC32","bidirectional":false}' \
-  # --quant False
+  --quant
 
 
 ##### copy from below for ease of access: ##### 
@@ -16,3 +16,5 @@ python sender_encode.py \
 # conv_lstm_PNC32_ae
 # conv_lstm_PNC32_ae_dropUpTo_32_final_pristine.pth
 # conv_lstm_PNC32_ae_dropUpTo_32_bidirectional_final.pth
+# PNC32_final_no_dropouts_quantize256_8bits.pth
+# conv_lstm_PNC32_ae_dropUpTo_32_quantize256_8_bits.pth
